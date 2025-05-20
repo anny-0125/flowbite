11fc50f5e8d8500fedb656dee38bc0af243b5862
anny <anny5432125@gmail.com>
2025-05-20 11:41:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,124 @@
+// var name ="sarmin";
+// var age ="20"
+// var x=20
+// var y=30
+
+// console.log(name+" is a js developer")
+// console.log(name+" is "+age+" years old")
+// console.log(name+x+y)
+// console.log(x+y)
+// var a =50
+// var b =30
+// sum = a+b
+// console.log(sum)
+
+
+// var string1 = "hello ";
+// var string2 = "world ";
+// var string3 = "good ";
+// var string4 = "morning"
+
+// var string = string1+string2+string3+string4;
+// console.log(string)
+
+// var a = 10;
+// var b = 20;
+// var sub = a-b;
+// console.log(sub)
+
+// var c = 60;
+// var d = 30;
+// var multi = c*d;
+// console.log(multi)
+
+// var e = 60;
+// var f = 30;
+// var divition = e/f
+// console.log(divition)
+
+// var num1 = 25;
+// var num2 = 3;
+// var result = 25%3;
+// console.log(result)
+
+// var h = 20;
+// console.log(++h)
+
+// var i = 30;
+// console.log(i++)
+// console.log(i)
+
+// var k = 30;
+// console.log(--k)
+
+// var j= 30;
+// console.log(j--)
+// console.log(j)
+
+// var base = 10;
+// var power = 5;
+// var res = base**power
+// console.log(res)
+// var x = 50;
+// var y = 20;
+//  x+=y
+//  console.log( var a = 80;
+// var b = 30;
+// a-=b
+// console.log(a)
+// var a = 3;
+// var b = 3;
+//  a**=b
+// console.log(a)
+// var a = 3;
+// var b = 5;
+// var compare = a != b;
+// console.log(compare)
+// var a = 3;
+// var b = 5;
+// var compare = a > b;
+// console.log(compare)
+// var a = 3;
+// var b = 5;
+// var compare = a < b;
+// console.log(compare)
+
+// var x = "500";
+// var y = 100
+// if( x == y){
+// console.log("anny is in the class now")
+// }
+// else{
+//     console.log("anny is not in class")
+// }
+// var office = "evening";
+// var officeTime;
+
+// switch(office){
+// case "morning":
+// officeTime = "9am"
+// break;
+// case "evening":
+// officeTime = "5pm"
+// break;
+// default:
+//  officeTime = "night"
+// }
+// console.log(officeTime)
+// var language = ["english","bangla","spanish","korean","arabic","japanese",10,20,true,false];
+// console.log(language)
+// language[6] = "garman"
+// console.log(language)
+// var cars = [];
+// cars [0] = "shohag";
+// cars [1] = "bmw";
+// cars [2] = "liton";
+// cars [3] = "hanif";
+// console.log(cars);
+// var cars = new Array("shohag","bmw","liton")
+// console.log(cars)
+const fruits = ["banana","orange","apple","mango"]
+// console.log(fruits);
+// console.log(fruits.push("cherry"));
+fruits[fruits.length] = "cherrry"
+console.log(fruits)
